cb7347e9b3bcfb4c2b9e9f34fb2c2034557f03bd
FernandoDLT <96254499+FernandoDLT@users.noreply.github.com>
2023-04-29 17:08:40 -0600

    init

---
+++ src/components/Hero.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function Hero() {
+   return (
+      <section className="hero">
+         <img src="group-77.png" alt="group" />
+         <h1 className="hero--header">Online Experience</h1>
+         <p className="hero--text">Join unique interactive activities led by
+            one-of-a-king host-all without leaving home</p>
+      </section>
+   )
+}
