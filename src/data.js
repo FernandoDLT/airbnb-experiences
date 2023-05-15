4bad3fd0021f8b1635484f322825ded3c597cb5c
FernandoDLT <96254499+FernandoDLT@users.noreply.github.com>
2023-05-15 16:37:49 -0600

    fixed margin for images added new items

--- src/data.js
+++ src/data.js
@@ -14,6 +14,19 @@ export default [
     },
     {
         id: 2,
+        title: "Explore the Antient Ruins",
+        description: "Interested in becoming a wedding photographer? For beginner and experienced photographers alike, join us in learning techniques required to leave the happy couple with memories that'll last a lifetime.",
+        price: 85,
+        coverImg: "tulum.png",
+        stats: {
+            rating: 5.0,
+            reviewCount: 30
+        },
+        location: "Tulum, Mex.",
+        openSpots: 27,
+    },
+    {
+        id: 3,
         title: "Learn Wedding Photography",
         description: "Interested in becoming a wedding photographer? For beginner and experienced photographers alike, join us in learning techniques required to leave the happy couple with memories that'll last a lifetime.",
         price: 125,
@@ -26,7 +39,20 @@ export default [
         openSpots: 27,
     },
     {
-        id: 3,
+        id: 4,
+        title: "Preestine Coast Line Getaway",
+        description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
+        price: 180,
+        coverImg: "balboa-beach.png",
+        stats: {
+            rating: 4.9,
+            reviewCount: 9
+        },
+        location: "Balboa Beach, USA",
+        openSpots: 3,
+    },
+    {
+        id: 5,
         title: "Group Mountain Biking",
         description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
         price: 50,
@@ -37,5 +63,44 @@ export default [
         },
         location: "Norway",
         openSpots: 3,
-    }
+    },
+    {
+        id: 6,
+        title: "Tequila Exprss Train Tour",
+        description: "I will share with you what I call \"Positively Impactful Moments of Disappointment.\" Throughout my career, many of my highest moments only came after setbacks and losses. But learning from those difficult moments is what gave me the ability to rise above them and reach my goals.",
+        price: 75,
+        coverImg: "tequila-logo.png",
+        stats: {
+            rating: 5.0,
+            reviewCount: 19
+        },
+        location: "Tequila, Mex.",
+        openSpots: 0,
+    },
+    {
+        id: 7,
+        title: "5 min from Ipanema Beach",
+        description: "Interested in becoming a wedding photographer? For beginner and experienced photographers alike, join us in learning techniques required to leave the happy couple with memories that'll last a lifetime.",
+        price: 60,
+        coverImg: "brazil.png",
+        stats: {
+            rating: 5.0,
+            reviewCount: 30
+        },
+        location: "Brazil",
+        openSpots: 27,
+    },
+    {
+        id: 8,
+        title: "Real de 14 old mining town",
+        description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
+        price: 50,
+        coverImg: "real-de-14.png",
+        stats: {
+            rating: 4.8,
+            reviewCount: 2
+        },
+        location: "Online",
+        openSpots: 3,
+    },
 ]
